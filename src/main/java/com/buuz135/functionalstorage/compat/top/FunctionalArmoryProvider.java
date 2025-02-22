e8783233fbdafcdf5022070a0bd3fe698719b94a
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2025-02-22 08:35:56 +0100

    [21.1] Armory Cabinet providers for Jade and TOP
    Displays a simple number that shows how many items that are currently stored in the Armory Cabinet

---
+++ src/main/java/com/buuz135/functionalstorage/compat/top/FunctionalArmoryProvider.java
@@ -0,0 +1,36 @@
+package com.buuz135.functionalstorage.compat.top;
+
+import com.buuz135.functionalstorage.FunctionalStorage;
+import com.buuz135.functionalstorage.block.config.FunctionalStorageConfig;
+import com.buuz135.functionalstorage.block.tile.ArmoryCabinetTile;
+import mcjty.theoneprobe.api.*;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.function.Function;
+
+public class FunctionalArmoryProvider implements IProbeInfoProvider {
+
+    public static final Function<ITheOneProbe, Void> REGISTER = iTheOneProbe -> {
+        iTheOneProbe.registerProvider(new FunctionalArmoryProvider());
+        return null;
+    };
+
+    @Override
+    public ResourceLocation getID() {
+        return com.buuz135.functionalstorage.util.Utils.resourceLocation(FunctionalStorage.MOD_ID, "armory");
+    }
+
+    @Override
+    public void addProbeInfo(ProbeMode probeMode, IProbeInfo iProbeInfo, Player player, Level level, BlockState blockState, IProbeHitData iProbeHitData) {
+        BlockEntity blockEntity = level.getBlockEntity(iProbeHitData.getPos());
+        if (blockEntity instanceof ArmoryCabinetTile armory) {
+            long count = armory.handler.stackList.stream().filter(stack -> !stack.isEmpty()).count();
+            iProbeInfo.vertical().mcText(Component.literal(count + " / " + FunctionalStorageConfig.ARMORY_CABINET_SIZE));
+        }
+    }
+}
