172610786b162a48a0ad74ba064917326202fdca
Satherov <satherov@users.noreply.github.com>
2025-10-03 15:32:28 +0200

    pls dont leak stuff here

--- src/main/java/com/buuz135/functionalstorage/compat/ftb/FTBChunksManager.java
+++ src/main/java/com/buuz135/functionalstorage/compat/ftb/FTBChunksManager.java
@@ -9,10 +9,8 @@ import dev.ftb.mods.ftbchunks.data.ClaimedChunkManagerImpl;
 
 public class FTBChunksManager {
 
-    private static final ClaimedChunkManagerImpl INSTANCE = ClaimedChunkManagerImpl.getInstance();
-
     public static boolean preventInteraction(BlockPos pos, Player player) {
-        return INSTANCE.shouldPreventInteraction(player, InteractionHand.MAIN_HAND, pos, Protection.INTERACT_BLOCK, null) ||
-                INSTANCE.shouldPreventInteraction(player, InteractionHand.OFF_HAND, pos, Protection.INTERACT_BLOCK, null);
+        return ClaimedChunkManagerImpl.getInstance().shouldPreventInteraction(player, InteractionHand.MAIN_HAND, pos, Protection.INTERACT_BLOCK, null) ||
+                ClaimedChunkManagerImpl.getInstance().shouldPreventInteraction(player, InteractionHand.OFF_HAND, pos, Protection.INTERACT_BLOCK, null);
     }
 }
